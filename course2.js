a1823650abb85d36c668b14db1f4fe534382708e
dragan992 <dragan_blok1@hotmail.com>
2021-05-27 22:20:18 +0200

    my new commit

---
+++ course2.js
@@ -0,0 +1,67 @@
+var number = 5; //in-line comment
+
+/* this is a
+multi-line comment */
+
+number = 9;
+
+/* Data Types:
+undefined, null, boolean, string, number, and object
+*/
+
+var myName = "Beau";
+
+myName = 8;
+
+let ourName = "freeCodeCamp";
+
+const pi = 3.14;
+
+var a;
+var b = 2;
+console.log(a);
+a = 7;
+
+b = a;
+
+console.log(a);
+
+//Declarations - declaring a variable
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+//Assignments - asigning a variable
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+var sum = 10 + 10;
+console.log(sum);
+
+var difference = 45 - 33;
+console.log(difference);
+
+var product = 8 * 10;
+console.log(product);
+
+var quotient= 66 / 33;
+console.log(quotient);
+
+
+//Dodavanje (prirastaj) brojeva
+
+var myVar = 87;
+
+// Umesto da kucam myVar= myVar + 1; kucati ovo dole
+
+myVar++;
+console.log(myVar);
+
+
+
+
+
+
